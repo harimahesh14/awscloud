52a9825f986b7dc336fc8275f24be6a7abbb6a9b
harimahesh14 <62423278+harimahesh14@users.noreply.github.com>
2020-06-16 20:28:08 +0530

    Create cloud.php

---
+++ cloud.php
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD>
+<title>Launching EC2 instance using terraform</title>
+<body bgcolor=Aqua>
+ HI!!!!!
+<img src="https://shailja85.s3.ap-south-1.amazonaws.com/shailja.jpg" width="1084" height="804">
+</body>
+</HEAD>
+</HTML>
